616f0135d0282c98ad8d8e10a28d6beb5c6fadd6
OlehYavoriv <oleh.yavoriv@gmail.com>
2023-12-05 15:50:55 +0200

    install and connect react-query

--- src/index.tsx
+++ src/index.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
+import queryClient from "./utils/queryClient";
+import { QueryClientProvider } from "react-query";
 import { App } from './App';
 import './styles/index.scss';
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
     <React.StrictMode>
-        <App/>
+        <QueryClientProvider client={queryClient}>
+            <App/>
+        </QueryClientProvider>
     </React.StrictMode>
 );
